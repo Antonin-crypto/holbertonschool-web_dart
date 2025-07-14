cc4b7e60d16c755447988350667b486e5ecc71df
Antonin-crypto <akamis53@gmail.com>
2025-07-14 09:14:46 +0200

    Majuscule Free

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,11 +1,11 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   // Calcul du score total de l'équipe A
-  int scoreA = (teamA['free throws'] ?? 0) * 1 +
+  int scoreA = (teamA['Free throws'] ?? 0) * 1 +
                (teamA['2 pointers'] ?? 0) * 2 +
                (teamA['3 pointers'] ?? 0) * 3;
 
   // Calcul du score total de l'équipe B
-  int scoreB = (teamB['free throws'] ?? 0) * 1 +
+  int scoreB = (teamB['Free throws'] ?? 0) * 1 +
                (teamB['2 pointers'] ?? 0) * 2 +
                (teamB['3 pointers'] ?? 0) * 3;
 
