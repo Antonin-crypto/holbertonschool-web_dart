e0542cc77c4c7f01e522e96c688523b59df349e3
Antonin-crypto <akamis53@gmail.com>
2025-07-14 09:12:26 +0200

    variable

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,18 +1,18 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   // Calcul du score total de l'équipe A
-  int totalA = (teamA['free throws'] ?? 0) * 1 +
+  int scoreA = (teamA['free throws'] ?? 0) * 1 +
                (teamA['2 pointers'] ?? 0) * 2 +
                (teamA['3 pointers'] ?? 0) * 3;
 
   // Calcul du score total de l'équipe B
-  int totalB = (teamB['free throws'] ?? 0) * 1 +
+  int scoreB = (teamB['free throws'] ?? 0) * 1 +
                (teamB['2 pointers'] ?? 0) * 2 +
                (teamB['3 pointers'] ?? 0) * 3;
 
   // Comparaison des scores
-  if (totalA > totalB) {
+  if (scoreA > scoreB) {
     return 1;
-  } else if (totalA < totalB) {
+  } else if (scoreA < scoreB) {
     return 2;
   } else {
     return 0;
