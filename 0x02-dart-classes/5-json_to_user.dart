bb03b3842ae5a9cbc12637ea5392368af13bd01a
Antonin-crypto <akamis53@gmail.com>
2025-07-15 09:44:24 +0200

    espace

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -22,6 +22,6 @@ class User {
 
   @override
   String toString() {
-    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
